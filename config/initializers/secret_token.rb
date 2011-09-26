3628ef77ef72dfd830728d596a61ed67d6ae3438
VR Engineering Pair <pair@host-137-6-0-10.sfoffice.verticalresponse.com>
2011-09-26 14:24:17 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MysqlBenchmark::Application.config.secret_token = 'bc9525cb3f3d55479dd09faadf1a7d47c077f4a8167d15190e20a02bff598f5cd8c7a9650b7dcf9fa4624ce0697c1ebd847144433152ecd59f0b499c3e8ad275'
